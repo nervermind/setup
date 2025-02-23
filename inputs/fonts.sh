6e71dfa532d9b46c82c9ebfe0bbe6426b45fb10d
nervermind <muha.hoihoi@me.com>
2025-02-24 00:24:25 +0100

    moved scripts to scripts secstion

--- inputs/fonts.sh
+++ inputs/fonts.sh
@@ -2,9 +2,3 @@ fonts=(
 	font-caskaydia-cove-nerd-font
     font-code-new-roman-nerd-font
 )
-
-install_fonts() {
-	info "Installing fonts..."
-	#brew tap homebrew/cask-fonts
-	install_brew_fonts "${fonts[@]}"
-}
