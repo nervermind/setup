d80cbc4d8098c978fce63060baccf24348f7822a
nervermind <muha.hoihoi@me.com>
2025-02-24 00:24:55 +0100

    moved mas to mas.sh

---
+++ scripts/mas.sh
@@ -0,0 +1,31 @@
+# Check for the --test flag
+if [[ " $* " == *" --test "* ]]; then
+  . ./inputs/apps.sh
+  . ./scripts/utils.sh 
+  info "Test mode running"
+  TEST=true
+  OS_VERSION=$(sw_vers -productVersion)
+fi
+
+install_masApps() {
+    if version_ge "$OS_VERSION" "14.2"; then
+    # Install mas apps
+        if [[ ${#mas_apps[@]} -gt 0 ]]; then
+            info "Installing App Store apps..."
+            for app in "${mas_apps[@]}"; do
+                mas install "$app"
+            done
+        else
+            info "No App Store apps to install."
+        fi
+    else
+        warn "Skipping mas apps installation. macOS version is less than 14.2"
+    fi
+}
+
+
+# Run the functions if the --test flag is set
+if [ "$TEST" = true ]; then
+  info "Testing Started"
+  install_masApps
+fi 
